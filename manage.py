4a6066415d16c8be729383e92860e77b30b8fcb3
zhoutao <zhoutao_wx@msn.com>
2021-07-28 10:08:39 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nnti.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "x.settings")
 
     from django.core.management import execute_from_command_line
 
